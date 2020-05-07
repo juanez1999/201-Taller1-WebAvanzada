6ee6a984073ffefc7f6e0fcd570f762cc340bc4a
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2020-05-07 11:39:03 -0500

    Updates

--- precache-manifest.248dd04f623720197dd4fea3a785ec1f.js
+++ precache-manifest.248dd04f623720197dd4fea3a785ec1f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08e45f9427d264d12e46c16b08828f5c",
+    "revision": "08ebb58dd986643f56865b75c9a2b14f",
     "url": "/201-Taller1-WebAvanzada/index.html"
   },
   {
-    "revision": "7ee5a8c10e04fc6cd97a",
+    "revision": "b1c341d952730b137e6c",
     "url": "/201-Taller1-WebAvanzada/static/css/main.fa00fa5f.chunk.css"
   },
   {
-    "revision": "bcec084b675f9f9692c5",
-    "url": "/201-Taller1-WebAvanzada/static/js/2.fb9742fe.chunk.js"
+    "revision": "5e354e275e0fd34c4589",
+    "url": "/201-Taller1-WebAvanzada/static/js/2.f09471b3.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/201-Taller1-WebAvanzada/static/js/2.fb9742fe.chunk.js.LICENSE.txt"
+    "url": "/201-Taller1-WebAvanzada/static/js/2.f09471b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7ee5a8c10e04fc6cd97a",
-    "url": "/201-Taller1-WebAvanzada/static/js/main.1831d84a.chunk.js"
+    "revision": "b1c341d952730b137e6c",
+    "url": "/201-Taller1-WebAvanzada/static/js/main.65eab6af.chunk.js"
   },
   {
     "revision": "185a5c6e1517de35f590",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/201-Taller1-WebAvanzada/precache-manifest.2151b1aee22d34cf986473046f52d0f9.js"
+  "/201-Taller1-WebAvanzada/precache-manifest.248dd04f623720197dd4fea3a785ec1f.js"
 );
 
 self.addEventListener('message', (event) => {
