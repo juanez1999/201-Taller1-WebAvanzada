160974eb643ef18f1d27b08949af288de7587f9f
Juan Esteban Pérez Trejos <juanez1999@gmail.com>
2020-05-07 11:25:45 -0500

    Updates

---
+++ precache-manifest.2151b1aee22d34cf986473046f52d0f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e45f9427d264d12e46c16b08828f5c",
+    "url": "/201-Taller1-WebAvanzada/index.html"
+  },
+  {
+    "revision": "7ee5a8c10e04fc6cd97a",
+    "url": "/201-Taller1-WebAvanzada/static/css/main.fa00fa5f.chunk.css"
+  },
+  {
+    "revision": "bcec084b675f9f9692c5",
+    "url": "/201-Taller1-WebAvanzada/static/js/2.fb9742fe.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/201-Taller1-WebAvanzada/static/js/2.fb9742fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ee5a8c10e04fc6cd97a",
+    "url": "/201-Taller1-WebAvanzada/static/js/main.1831d84a.chunk.js"
+  },
+  {
+    "revision": "185a5c6e1517de35f590",
+    "url": "/201-Taller1-WebAvanzada/static/js/runtime-main.32f1024d.js"
+  }
+]);
